8f86bf20069dc4367d85d80df8cd3acb6e0688dc
lufti <luftinurfahmi@gmail.com>
2014-07-26 04:28:06 +0700

    First initial

---
+++ gen/com/luftinurfahmi/net/SlideDownCart/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.luftinurfahmi.net.SlideDownCart;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
---
+++ gen/com/luftinurfahmi/net/SlideDownCart/R.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.luftinurfahmi.net.SlideDownCart;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+    public static Object layout;
+}
